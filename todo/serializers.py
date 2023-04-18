798d02f6dec57d87b1ca480c2f18c60314ce1b66
SCollins <sje.collins@gmail.com>
2023-04-18 20:37:55 +0000

    More serializers

--- todo/serializers.py
+++ todo/serializers.py
@@ -1,3 +1,4 @@
+from django.contrib.humanize.templatetags.humanize import naturaltime
 from rest_framework import serializers
 from .models import Project, Task
 
@@ -5,11 +6,19 @@ from .models import Project, Task
 class ProjectSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
     is_owner = serializers.SerializerMethodField()
+    created = serializers.SerializerMethodField()
+    deadline = serializers.SerializerMethodField()
 
     def get_is_owner(self, obj):
         request = self.context['request']
         return request.user == obj.owner
-    
+
+    def get_created(self, obj):
+        return naturaltime(obj.created)
+
+    def get_deadline(self, obj):
+        return naturaltime(obj.deadline)
+
     class Meta:
         model = Project
         fields = [
@@ -20,14 +29,30 @@ class ProjectSerializer(serializers.ModelSerializer):
 class TaskSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
     is_owner = serializers.SerializerMethodField()
+    added = serializers.SerializerMethodField()
+    updated = serializers.SerializerMethodField()
+    due = serializers.SerializerMethodField()
 
     def get_is_owner(self, obj):
         request = self.context['request']
         return request.user == obj.owner
 
+    def get_added(self, obj):
+        return naturaltime(obj.added)
+
+    def get_updated(self, obj):
+        return naturaltime(obj.updated)
+
+    def get_due(self, obj):
+        return naturaltime(obj.due)
+
     class Meta:
         model = Task
         fields = [
             'id', 'owner', 'is_owner', 'name', 'description', 'priority',
-            'status', 'approved', 'added', 'updated', 'due'
+            'status', 'approved', 'added', 'updated', 'due', 'project'
         ]
+
+
+class TaskDetailSerializer(TaskSerializer):
+    project = serializers.ReadOnlyField(source='project.id')
--- todo/views.py
+++ todo/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from reactkanban.permissions import IsOwnerOrReadOnly
+from .models import Project, Task
+from .serializers import ProjectSerializer, TaskSerializer, TaskDetailSerializer
 
-# Create your views here.
+
+class ProjectList(generics.ListCreateAPIView):
+    serializer_class = ProjectSerializer
+    queryset = Project.objects.all()
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+
+class ProjectDetail(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = [IsOwnerOrReadOnly]
+    serializer_class = ProjectSerializer
+    queryset = Project.objects.all()
+
+
+class TaskList(generics.ListCreateAPIView):
+    serializer_class = TaskSerializer
+    queryset = Task.objects.all()
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+
+class TaskDetail(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = [IsOwnerOrReadOnly]
+    serializer_class = TaskDetailSerializer
+    queryset = Task.objects.all()
